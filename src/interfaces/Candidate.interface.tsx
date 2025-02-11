cc087bfdf0f66c2a3d3f64ae033fc01b01fd9bec
gerdesjasonl <gerdesjasonl@gmail.com>
2025-02-11 09:40:19 -0500

    CandidateSearch basics, fetch not currently working

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,5 +1,7 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 interface CandidateProps {
+    Img: string;
+    Name: string;
     Location: string;
     Email: string;
     Company: string;
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,41 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import Candidate from '../interfaces/Candidate.interface';
 
-const CandidateSearch = () => {
-  return <h1>Candidate Search</h1>;
+const CandidateSearch: React.FC = () => {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  
+  useEffect (() => {
+    const fetchCandidates = async () => {
+      try {
+        const response = await searchGithub();
+        if (!response.ok) throw new Error("Failed to fetch data");
+
+        const candidatesData: Candidate[] = await response.json();
+        setCandidates(candidatesData);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "Unknown error");
+      }
+    };
+    fetchCandidates();
+  }, []);
+  
+  if (error) return <p>Error: {error}</p>
+
+  return (
+    <ul>
+      {candidates.map((candidate) => (
+        <li key={candidate.Name}>
+          <p><strong>Name:</strong>{candidate.Name}</p>
+          <p><strong>Location:</strong>{candidate.Location}</p>
+          <p><strong>Email:</strong> {candidate.Email}</p>
+          <p><strong>Company:</strong> {candidate.Company}</p>
+          <p><strong>Bio:</strong> {candidate.Bio}</p>
+        </li>
+      ))}
+    </ul>
+    );
 };
 
 export default CandidateSearch;
