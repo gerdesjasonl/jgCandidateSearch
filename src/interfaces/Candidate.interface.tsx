ceb9691ede5c572d437c35400bfaab8b6eb05cbe
gerdesjasonl <gerdesjasonl@gmail.com>
2025-02-10 19:28:50 -0500

    This adds the candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,20 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface CandidateProps {
+    Location: string;
+    Email: string;
+    Company: string;
+    Bio: string;
+};
+
+function Candidate(props: CandidateProps) {
+    return (
+        <div className="candidate">
+            <p>{props.Location}</p>
+            <p>{props.Email}</p>
+            <p>{props.Company}</p>
+            <p>{props.Bio}</p>
+        </div>
+    )
+}
+
+export default Candidate;
